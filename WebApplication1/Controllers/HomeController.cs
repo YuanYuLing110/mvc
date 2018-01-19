09350d5b17f14122c5bd8cf221fac5dbdcfe13d1
Yuan <YuanYuLing110@126.com>
2018-01-19 16:46:55 +0800

    ef

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplication1.DAL;
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        efEntities tc = new efEntities();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
@@ -22,8 +24,33 @@ namespace WebApplication1.Controllers
         {
             //txt_ip = "127.0.0.1";
             if (btn == "确定")
-              
-                 return RedirectToAction("Result", "Home", new { txt=txt_ip});
+            {
+                var sysUsers = new List<DAL.table_ip>
+                {
+                new DAL.table_ip {ip="127.0.0.1",city="aaaa"}
+
+                };
+                System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex(@"\d");
+                reg.Replace("ss3ss6a21ad342s", new System.Text.RegularExpressions.MatchEvaluator(
+                    (System.Text.RegularExpressions.Match match) => {
+
+                    var number = int.Parse(match.Value);
+                    if (number % 2 == 0)
+                        return "]";
+                    else
+                        return "[";
+                }));
+                
+                sysUsers.ForEach(s => tc.table_ip.Add(s));
+                tc.SaveChanges();
+                var td = from table_ip_ in tc.table_ip where table_ip_.ip == "" select new { table_ip_.id };
+                td.FirstOrDefault();
+                //var ip_city = tc.city.Where("");
+                List<user> users = new List<user>(10000);
+
+                return RedirectToAction("Result", "Home", new { txt = txt_ip });
+            }
+                 
             else if(btn=="取消")
             {
                 ViewBag.tit = "Home Page";              
@@ -49,9 +76,8 @@ namespace WebApplication1.Controllers
 
             StreamReader sr = new StreamReader(stream, Encoding.Default);
             string s = sr.ReadToEnd();
-            //textBox1.Text = s;
-            //webBrowser1.DocumentText = s;
-            //ViewBag.city = s;
+           
+
             ViewBag.city = s;
             sr.Close();
 
---
+++ demo2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "a c v o e";
+
+             System.Text.RegularExpressions.Regex reg=new System.Text.RegularExpressions.Regex("");
+             reg.Replace(" ", new System.Text.RegularExpressions.MatchEvaluator((System.Text.RegularExpressions.Match match) =>
+             {
+
+
+             }));
+        }
+    }
+}
